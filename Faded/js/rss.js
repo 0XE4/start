8ef2df512cf9f3055d5605baf9fae2cc4a8d23cb
Mathias Busto <0XE4@users.noreply.github.com>
2016-12-16 00:17:26 +0000

    Link change and RSS addition

---
+++ Faded/js/rss.js
@@ -0,0 +1,26 @@
+function RSSWidget(a) {
+    rss = this, rss.FEED_URL = a, rss.JSON = new Array, rss.widgetHolder = $(".rss-widget ul"), rss.storiesLimit = 2, $.ajax({
+        url: "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=" + encodeURIComponent(rss.FEED_URL),
+        dataType: "json",
+        success: function(a) {
+            if (a.responseData.feed && a.responseData.feed.entries) {
+                $.each(a.responseData.feed.entries, function(a, b) {
+                    rss.JSON.push({
+                        title: b.title,
+                        author: b.author,
+                        content: b.content || "",
+                        link: b.link
+                    })
+                }), rss.storiesLimit > rss.JSON.length && (rss.storiesLimit = rss.JSON.length);
+                for (var b = 0; b < rss.storiesLimit; b++) rss.renderBlogItem(rss.JSON[b]);
+                $(".rss-widget gg").each(function() {
+                    $(this).index() / rss.storiesLimit + "s"
+                })
+            }
+        }
+    }), rss.renderBlogItem = function(a) {
+        var b = '<gg class="blog-item">';
+        b += '<div class="blog-item-title">' + a.title + "</div>", b += "</gg>", rss.widgetHolder.append(b)
+    }
+}
+RSSWidget("https://twitrss.me/twitter_user_to_rss/?user=AlanWattsDaily");
